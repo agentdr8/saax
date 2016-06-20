19eb40a8242742f039c970f9103147fec599bc36
dr8 <dr8pwnzu@gmail.com>
2016-06-20 10:38:29 -0700

    Fix .gitignore issue

---
+++ saax_module/src/main/java/com/dr8/xposed/saax/PrefsActivity.java
@@ -0,0 +1,79 @@
+package com.dr8.xposed.saax;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+public class PrefsActivity extends PreferenceActivity {
+
+    SharedPreferences prefs = null;
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setTitle(R.string.app_name);
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        if (savedInstanceState == null)
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+
+        prefs = getSharedPreferences("com.dr8.xposed.saax_preferences", MODE_WORLD_READABLE);
+        isFirstRun();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    private void isFirstRun() {
+        if (prefs.getBoolean("firstrun", true)) {
+            showDialog(this, getString(R.string.alert_title), getText(R.string.disclaimer)).show();
+        }
+    }
+
+    public Dialog showDialog(Activity activity, String title, CharSequence message) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        if (title != null) builder.setTitle(title);
+
+        builder.setMessage(message)
+                .setPositiveButton("I Agree", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        prefs.edit().putBoolean("firstrun", false).apply();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @SuppressWarnings("deprecation")
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // this is important because although the handler classes that read these settings
+            // are in the same package, they are executed in the context of the hooked package
+            getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+}
---
+++ saax_module/src/main/java/com/dr8/xposed/saax/Xposed.java
@@ -0,0 +1,101 @@
+package com.dr8.xposed.saax;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Xposed implements IXposedHookLoadPackage, IXposedHookZygoteInit {
+
+    public static boolean DEBUG = false;
+    private static String TAG = "SAAX: ";
+    private static XSharedPreferences prefs;
+
+    public static void log(String tag, String msg) {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        String formattedDate = df.format(c.getTime());
+        XposedBridge.log("[" + formattedDate + "] " + tag + ": " + msg);
+    }
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        prefs = new XSharedPreferences("com.dr8.xposed.saax", "com.dr8.xposed.saax_preferences");
+        prefs.makeWorldReadable();
+        DEBUG = prefs.getBoolean("debug", false);
+    }
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        String targetpkg = "com.google.android.projection.gearhead";
+        String targetcls = "com.google.android.gsf.e";
+        String targetcsecls = "com.google.android.gms.car.CarSensorEvent";
+
+
+        if (lpparam.packageName.equals(targetpkg)) {
+            if (DEBUG) log(TAG, "Hooked Android Auto package");
+
+            Class<?> CSEcls = XposedHelpers.findClass(targetcsecls, lpparam.classLoader);
+            XposedBridge.hookAllConstructors(CSEcls, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    int b = (int) param.args[1];
+                    switch (b) {
+                        case 6:
+                            if (DEBUG) log(TAG, "Dropped CSE instance. b was " + b + " (ParkingBrakeData)");
+                            param.setResult(null);
+                            break;
+                        case 7:
+                            if (DEBUG) log(TAG, "Dropped CSE instance. b was " + b + " (GearData)");
+                            param.setResult(null);
+                            break;
+                        default:
+                    }
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(targetcls, lpparam.classLoader, "a", String.class, Integer.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            prefs.reload();
+                            String s = (String) param.args[0];
+                            int i = (int) param.args[1];
+                            if (s.equals("gearhead:max_forward_clicks")) {
+                                if (DEBUG) log(TAG, s + " is currently: " + i);
+                                param.args[1] = prefs.getInt("pref_maxTaps", 6);
+                                if (DEBUG) log(TAG, "setting " + s + " to " +
+                                        prefs.getInt("pref_maxTaps", 6));
+                            }
+                        }
+                    });
+
+            XposedHelpers.findAndHookMethod(targetcls, lpparam.classLoader, "a", String.class, Float.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            prefs.reload();
+                            String s = (String) param.args[0];
+                            float f = (float) param.args[1];
+                            if (s.equals("gearhead:max_speed_unlimited_browsing") ||
+                                    s.equals("gearhead:max_speed_parking_card")) {
+                                if (DEBUG) log(TAG, s + " is currently: " + f);
+                                float tmpflt = (float) prefs.getInt("pref_maxSpeed", 150) * 0.44704F;
+                                param.args[1] = tmpflt;
+                                if (DEBUG) log(TAG, "setting " + s + " to " +
+                                        tmpflt);
+                            }
+                        }
+                    });
+
+        }
+
+    }
+}
