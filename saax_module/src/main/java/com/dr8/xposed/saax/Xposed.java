b82702ccbc14a0e0854e480f5d58740c9168201a
Agentdr8 <dr8pwnzu@gmail.com>
2017-09-16 11:59:58 -0700

    Change class type for methodhook

--- saax_module/src/main/java/com/dr8/xposed/saax/Xposed.java
+++ saax_module/src/main/java/com/dr8/xposed/saax/Xposed.java
@@ -66,7 +66,7 @@ public class Xposed implements IXposedHookLoadPackage, IXposedHookZygoteInit {
                         }
                     });
 
-            XposedHelpers.findAndHookMethod(targetcls, lpparam.classLoader, "i", String.class, Boolean.class,
+            XposedHelpers.findAndHookMethod(targetcls, lpparam.classLoader, "i", String.class, boolean.class,
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
